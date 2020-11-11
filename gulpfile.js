6948cc567ce007446ac3f3a812fd3ba492024d98
Rachit Gulati <rachitgulati@jajacMScomp.redmond.corp.microsoft.com>
2020-11-12 03:40:26 +0530

    Renamed everything

---
+++ gulpfile.js
@@ -0,0 +1,54 @@
+'use strict';
+
+var gulp = require('gulp');
+const imagemin = require('gulp-imagemin');
+var sass = require('gulp-sass');
+var uglify = require('gulp-uglify');
+var rename = require('gulp-rename');
+var del = require('del');
+
+gulp.task('resize', function () {
+    return gulp.src('images/*.*')
+        .pipe(imagemin({
+            width: 1024,
+            imageMagick: true
+        }))
+        .pipe(gulp.dest('images/fulls'))
+        .pipe(
+            imagemin([
+            imagemin.mozjpeg({quality: 50, progressive: true})
+            ])
+        )
+        .pipe(gulp.dest('images/thumbs'));
+});
+
+gulp.task('del', ['resize'], function () {
+    return del(['images/*.*']);
+});
+
+// compile scss to css
+gulp.task('sass', function () {
+    return gulp.src('./assets/sass/main.scss')
+        .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+        .pipe(rename({basename: 'main.min'}))
+        .pipe(gulp.dest('./assets/css'));
+});
+
+// watch changes in scss files and run sass task
+gulp.task('sass:watch', function () {
+    gulp.watch('./assets/sass/**/*.scss', ['sass']);
+});
+
+// minify js
+gulp.task('minify-js', function () {
+    return gulp.src('./assets/js/main.js')
+        .pipe(uglify())
+        .pipe(rename({basename: 'main.min'}))
+        .pipe(gulp.dest('./assets/js'));
+});
+
+// default task
+gulp.task('default', ['del']);
+
+// scss compile task
+gulp.task('compile-sass', ['sass', 'minify-js']);
